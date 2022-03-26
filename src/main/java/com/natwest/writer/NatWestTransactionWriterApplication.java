adec05f271d3a4f02a4fa42d7619233582f937aa
Divyanshu <divyanshu.singh@enkash.com>
2022-03-26 17:48:40 +0530

    Initial commit of writer microservice

---
+++ src/main/java/com/natwest/writer/NatWestTransactionWriterApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatWestTransactionWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatWestTransactionWriterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/natwest/writer/NatWestTransactionWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatWestTransactionWriterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
